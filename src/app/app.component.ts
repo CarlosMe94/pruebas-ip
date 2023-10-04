4ea369dbab42011c025b3cb3d61b938c3cb9bb85
Carlos WSL <cmg@wsl.esL>
2023-10-04 21:01:50 +0200

    get privates ip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+declare global {
+  interface Window {
+    RTCPeerConnection: RTCPeerConnection;
+    mozRTCPeerConnection: RTCPeerConnection;
+    webkitRTCPeerConnection: RTCPeerConnection;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebas-ip';
+
+  localIp = sessionStorage.getItem('LOCAL_IP');
+
+  private ipRegex = new RegExp(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/);
+
+  constructor (private zone: NgZone) {
+  }
+
+  ngOnInit() {
+    this.determineLocalIp();
+  }
+
+  private determineLocalIp() {
+    window.RTCPeerConnection = this.getRTCPeerConnection();
+
+    const pc = new RTCPeerConnection({ iceServers: [] });
+    pc.createDataChannel('');
+    pc.createOffer().then(pc.setLocalDescription.bind(pc));
+
+    pc.onicecandidate = (ice) => {
+      this.zone.run(() => {
+        if (!ice || !ice.candidate || !ice.candidate.candidate) {
+          return;
+        }
+
+        this.localIp = this.ipRegex.exec(ice.candidate.candidate)![1];
+        sessionStorage.setItem('LOCAL_IP', this.localIp);
+
+        pc.onicecandidate = () => {};
+        pc.close();
+      });
+    };
+  }
+
+  private getRTCPeerConnection() {
+    return window.RTCPeerConnection ||
+      window.mozRTCPeerConnection ||
+      window.webkitRTCPeerConnection;
+  }
 }
